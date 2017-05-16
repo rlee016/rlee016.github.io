41fbf1a9dcaa4ab844305b5fcc662f8e5da42f74
jaju33 <jaju33@hotmail.com>
2017-05-15 21:35:36 -0400

    add changes

--- tictactoe.js
+++ tictactoe.js
@@ -4,3 +4,39 @@ function chooseXorO(symbol){
  choosenSymbol = symbol;
  document.getElementById("notes").innerHTML=choosenSymbol + " is playing!";
 }
+
+function makeMove(divLocation){
+  
+  var box = document.getElementById(divLocation).innerHTML;
+  
+  if(box === ""){
+    document.getElementById(divLocation).innerHTML=choosenSymbol;
+   }
+  
+  checkWinner();
+  } 
+
+function checkWinner(){ 
+  
+  var r1c1 = document.getElementById("row1col1").innerHTML;
+  var r1c2 = document.getElementById("row1col2").innerHTML;
+  var r1c3 = document.getElementById("row1col3").innerHTML;
+  var r2c1 = document.getElementById("row2col1").innerHTML;
+  var r2c2 = document.getElementById("row2col2").innerHTML;
+  var r2c3 = document.getElementById("row2col3").innerHTML;
+  var r3c1 = document.getElementById("row3col1").innerHTML;
+  var r3c2 = document.getElementById("row3col2").innerHTML;
+  var r3c3 = document.getElementById("row3col3").innerHTML;
+  
+  if(r1c1 == r2c1 && r1c1 == r3c1 && r1c1 !== "" ||
+     r1c2 == r2c2 && r1c2 == r3c2 && r1c2 !== "" ||
+     r1c3 == r2c3 && r1c3 == r3c3 && r1c3 !== "" ||
+     r1c1 == r1c2 && r1c1 == r1c3 && r1c1 !== "" ||
+     r2c1 == r2c2 && r2c1 == r2c3 && r2c1 !== "" ||
+     r3c1 == r3c2 && r3c1 == r3c3 && r3c1 !== "" ||
+     r1c1 == r2c2 && r1c1 == r3c3 && r1c1 !== "" ||
+     r1c3 == r2c2 && r1c3 == r3c1 && r1c3 !== "" ){
+    document.getElementById("notes").innerHTML=choosenSymbol + " is the winner";
+  }
+  
+}
